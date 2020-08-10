9aec37c068e8ba28f7c780b5a9876ff2815a4c04
Biswa <biswapriyah@gmail.com>
2020-08-10 22:35:46 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [resourcetype, setResourceType] = useState('');
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  }, [])
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourcetype}`)
+      .then(response => response.json())
+      .then(json => console.log(json))
+
+    return () => {
+      console.log('return from resource change')
+    }
+  }, [resourcetype])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{count}</p>
+      <button onClick={() => setResourceType('posts')}>Post</button>
+      <button onClick={() => setResourceType('comments')}>Comments</button>
+      <button onClick={() => setResourceType('users')}>Users</button>
+      <button onClick={() => setCount(count + 1)}>Click Me</button>
+      <h1>{resourcetype}</h1>
     </div>
   );
 }
